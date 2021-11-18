5fb75725addbee9a025f45b90c880b2bdeffc6f8
farhanakbarr008 <farhanakbarr008@gmail.com>
2021-11-18 20:12:56 +0700

    mencoba

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className='h-screen bg-gray-50 flex items-center justify-center'>
-      IuM App
+      SAMLEKOM MAMANK
     </div>
   );
 };
