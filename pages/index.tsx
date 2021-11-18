e48033e80d13758d2d8fe47027031578d5a6969d
farhanakbarr008 <farhanakbarr008@gmail.com>
2021-11-18 21:01:28 +0700

    iGracias clone

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className='h-screen bg-gray-50 flex items-center justify-center'>
-      UNDER MAINTENANCE
+    <div className='h-screen bg-[#b91432] text-white flex items-start justify-top pl-10 pt-5 gap-3'>
+      <img
+        src='https://igracias.telkomuniversity.ac.id/styles/dashboard2018/img/igracias2.png'
+        alt=''
+      />
+      DASHBOARD
     </div>
   );
 };
