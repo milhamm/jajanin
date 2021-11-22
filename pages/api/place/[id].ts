91a1fbdc2e032e1a9c75b098bc863c3ee18f7f53
farhanakbarr008 <farhanakbarr008@gmail.com>
2021-11-22 22:23:55 +0700

    update store_index and add detail and update store

--- pages/api/place/[id].ts
+++ pages/api/place/[id].ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../client/prisma";
+import { errorHandler } from "../../../helper/errorHandler";
 
 export default async function handler(
   req: NextApiRequest,
@@ -14,5 +15,37 @@ export default async function handler(
     });
 
     res.status(204).end();
+  } else if (req.method === "GET") {
+    const id = req.query.id as string;
+    const place = await prisma.store.findFirst({
+      where: {
+        slug: id,
+      },
+    });
+    res.json({
+      success: true,
+      code: 200,
+      data: place,
+    });
+  } else if (req.method === "PUT") {
+    const id = req.query.id as string;
+    const data = req.body;
+    try {
+      const place = await prisma.store.update({
+        where: {
+          id: id,
+        },
+        data: {
+          ...data,
+        },
+      });
+      res.json({
+        success: true,
+        code: 200,
+        data: place,
+      });
+    } catch (error) {
+      errorHandler(error, req, res);
+    }
   }
 }
