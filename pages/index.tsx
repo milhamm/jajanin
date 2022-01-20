5b6e837b6080e9eb4641c59e9e0daabcec5c8631
Muhammad Furqon Fahlevi <furqonfahlevirm@gmail.com>
2022-01-20 11:43:35 +0700

    responsive main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { IoLocationSharp, IoSearch } from "react-icons/io5";
 import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
+import { HiOutlineMenu } from "react-icons/hi";
 import type { NextPage } from "next";
 import RestaurantReview from "../components/RestaurantReview";
 import CollectionCard from "../components/CollectionCard";
@@ -10,30 +11,36 @@ const Home: NextPage = () => {
   return (
     <>
       {/* Header */}
+      {/* Header - Login */}
       <div className='w-full bg-red-500'>
-        <div className='container flex justify-end gap-4 p-5'>
+        <div className='container flex items-center justify-end gap-4 p-5 mobile:justify-start'>
+          <button>
+            <HiOutlineMenu className='text-white text-[2rem]' />
+          </button>
           <Link href='/auth/login'>
             <a>
-              <button className='text-white font-semibold px-3 py-1 rounded-lg hover:bg-white hover:text-red-500 '>
+              <button className='text-white font-semibold px-3 py-1 rounded-lg hover:bg-white hover:text-red-500 mobile:hidden'>
                 Log in
               </button>
             </a>
           </Link>
-          <button className='text-white font-semibold px-3 py-1 rounded-lg hover:bg-white hover:text-red-500'>
+          <button className='text-white font-semibold px-3 py-1 rounded-lg hover:bg-white hover:text-red-500 mobile:hidden'>
             Sign up
           </button>
         </div>
       </div>
-      <div className='flex items-center justify-start flex-col w-full h-[320px] bg-red-500'>
+      {/* Header - Logo */}
+      <div className='flex items-center justify-start flex-col w-full h-[320px] bg-red-500 pt-10'>
         <h1 className='font-["Mazzard"] text-[70px] text-white'>Jajanin</h1>
         <p className='text-white'>Discover the best food & drinks</p>
-        <div className='flex items-center gap-4 bg-white rounded-lg p-1 mt-4 sm:w-[75%] lg:w-[715px]'>
+        {/* Header - Search Bar */}
+        <div className='flex items-center gap-4 w-[715px] bg-white rounded-lg p-1 mt-4 mobile:w-[80%]'>
           <div className='flex items-center w-[100px] gap-2'>
-            <IoLocationSharp className='text-2xl fill-current text-red-500' />{" "}
+            <IoLocationSharp className='text-2xl fill-current text-red-500 mobile:text-xl' />{" "}
             Solo
           </div>
           <div className='flex items-center grow w-full gap-2'>
-            <IoSearch className='text-2xl fill-current text-red-500' />
+            <IoSearch className='text-2xl fill-current text-red-500 mobile:text-xl' />
             <input
               type='text'
               placeholder='Search for restaurant, cuisine or a dish'
@@ -43,16 +50,17 @@ const Home: NextPage = () => {
         </div>
       </div>
       {/* Slide Review */}
-      <div className='w-[900px] mx-auto my-0 mt-16 sm:w-full'>
-        <div className=' flex items-center justify-between mx-auto gap-[60px] sm:flex-col lg:flex'>
+      <div className='w-[900px] mx-auto my-0 mt-16 mobile:w-full mobile:container'>
+        <div className=' flex items-center justify-between mx-auto gap-[60px] mobile:flex-col'>
           <div className='h-[300px] w-[300px] rounded-lg shadow-lg shadow-black/15'></div>
-          <div className='w-[450px] rounded-lg shadow-lg shadow-gray-300/50'>
+          <div className='w-[450px] rounded-lg shadow-lg shadow-gray-300/50 mobile:w-full mobile:flex mobile:flex-col'>
             <RestaurantReview restaurantName='MCD' customerName={"Lepi"} />
             <RestaurantReview restaurantName='HISANA' customerName={"Raka"} />
             <RestaurantReview restaurantName='KFC' customerName={"Aang"} />
           </div>
         </div>
-        <div className='flex justify-end items-center py-2 gap-4 sm:justify-center'>
+        {/* Slide Review - Arrow button */}
+        <div className='flex justify-end items-center py-2 gap-4 mobile:justify-center'>
           <button>
             <BsArrowLeftCircle className='fill-current text-red-500 text-2xl' />
           </button>
@@ -64,16 +72,19 @@ const Home: NextPage = () => {
       {/* Collection */}
       <div className='container my-0 mt-5'>
         <h1 className='font-bold text-2xl'>Collections</h1>
-        <div className='flex justify-between items-center sm:text-sm'>
-          <p className='w-[60%]'>
+        <div className='flex justify-between items-center mobile:text-xs'>
+          <p className='w-[60%] mobile:w-[50%]'>
             Explore curated lists of top restaurants, cafes, pubs, and bars in
             Solo, based on trends
           </p>
-          <a href='' className='text-red-500 hover:underline'>
+          <a
+            href=''
+            className='text-red-500 hover:underline mobile:w-[50%] text-right'
+          >
             All collections in Solo
           </a>
         </div>
-        <div className='grid grid-cols-4 gap-3 mt-8 mb-4 sm:grid-cols-2'>
+        <div className='grid grid-cols-4 gap-3 mt-8 mb-4 mobile:grid-cols-2'>
           <CollectionCard
             collectionName='Trending this week'
             totalCollection={30}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
     extend: {
       screens: {
         mobile: "320px",
+        tablet: "481px",
+        laptop: "769px",
+        desktop: "1025px",
+        tv: "1201px",
       },
     },
   },
