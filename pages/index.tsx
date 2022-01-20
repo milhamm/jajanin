cb4a47595d88f7be0039d837f8f2dd688bcc3494
Muhammad Furqon Fahlevi <furqonfahlevirm@gmail.com>
2022-01-20 12:41:52 +0700

    fixing responsive main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { IoLocationSharp, IoSearch } from "react-icons/io5";
-import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
+import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 import { HiOutlineMenu } from "react-icons/hi";
 import type { NextPage } from "next";
 import RestaurantReview from "../components/RestaurantReview";
@@ -13,18 +13,18 @@ const Home: NextPage = () => {
       {/* Header */}
       {/* Header - Login */}
       <div className='w-full bg-red-500'>
-        <div className='container flex items-center justify-end gap-4 p-5 mobile:justify-start'>
+        <div className='container flex items-center gap-4 p-5 mobile:justify-start laptop:justify-end'>
           <button>
-            <HiOutlineMenu className='text-white text-[2rem]' />
+            <HiOutlineMenu className='text-white text-[2rem] laptop:hidden' />
           </button>
           <Link href='/auth/login'>
             <a>
-              <button className='text-white font-semibold px-3 py-1 rounded-lg hover:bg-white hover:text-red-500 mobile:hidden'>
+              <button className='text-white font-semibold px-3 py-1 rounded-lg hover:bg-white hover:text-red-500 mobile:hidden laptop:block'>
                 Log in
               </button>
             </a>
           </Link>
-          <button className='text-white font-semibold px-3 py-1 rounded-lg hover:bg-white hover:text-red-500 mobile:hidden'>
+          <button className='text-white font-semibold px-3 py-1 rounded-lg hover:bg-white hover:text-red-500 mobile:hidden laptop:block'>
             Sign up
           </button>
         </div>
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
         <h1 className='font-["Mazzard"] text-[70px] text-white'>Jajanin</h1>
         <p className='text-white'>Discover the best food & drinks</p>
         {/* Header - Search Bar */}
-        <div className='flex items-center gap-4 w-[715px] bg-white rounded-lg p-1 mt-4 mobile:w-[80%]'>
+        <div className='flex items-center gap-4 bg-white rounded-lg p-1 mt-4 mobile:w-[80%] laptop:w-[715px]'>
           <div className='flex items-center w-[100px] gap-2'>
             <IoLocationSharp className='text-2xl fill-current text-red-500 mobile:text-xl' />{" "}
             Solo
@@ -50,29 +50,29 @@ const Home: NextPage = () => {
         </div>
       </div>
       {/* Slide Review */}
-      <div className='w-[900px] mx-auto my-0 mt-16 mobile:w-full mobile:container'>
-        <div className=' flex items-center justify-between mx-auto gap-[60px] mobile:flex-col'>
+      <div className='mx-auto my-0 mt-16 mobile:w-full mobile:container tablet:px-[1rem] laptop:w-[900px]'>
+        <div className=' flex items-center justify-between mx-auto gap-[60px] mobile:flex-col laptop:flex-row'>
           <div className='h-[300px] w-[300px] rounded-lg shadow-lg shadow-black/15'></div>
-          <div className='w-[450px] rounded-lg shadow-lg shadow-gray-300/50 mobile:w-full mobile:flex mobile:flex-col'>
+          <div className='w-[450px] rounded-lg shadow-lg shadow-gray-300/50 mobile:w-full mobile:flex mobile:flex-col laptop:w-[450px]'>
             <RestaurantReview restaurantName='MCD' customerName={"Lepi"} />
             <RestaurantReview restaurantName='HISANA' customerName={"Raka"} />
             <RestaurantReview restaurantName='KFC' customerName={"Aang"} />
           </div>
         </div>
         {/* Slide Review - Arrow button */}
-        <div className='flex justify-end items-center py-2 gap-4 mobile:justify-center'>
+        <div className='flex items-center py-2 gap-4 mobile:justify-center laptop:justify-end'>
           <button>
-            <BsArrowLeftCircle className='fill-current text-red-500 text-2xl' />
+            <BsArrowLeftShort className='fill-current text-red-500 text-2xl rounded-full border border-red-500 hover:bg-red-500 hover:text-white hover:border-transparent' />
           </button>
           <button>
-            <BsArrowRightCircle className='fill-current text-red-500 text-2xl' />
+            <BsArrowRightShort className='fill-current text-red-500 text-2xl rounded-full border border-red-500 hover:bg-red-500 hover:text-white hover:border-transparen' />
           </button>
         </div>
       </div>
       {/* Collection */}
       <div className='container my-0 mt-5'>
         <h1 className='font-bold text-2xl'>Collections</h1>
-        <div className='flex justify-between items-center mobile:text-xs'>
+        <div className='flex justify-between items-center mobile:text-xs laptop:text-sm'>
           <p className='w-[60%] mobile:w-[50%]'>
             Explore curated lists of top restaurants, cafes, pubs, and bars in
             Solo, based on trends
@@ -84,7 +84,7 @@ const Home: NextPage = () => {
             All collections in Solo
           </a>
         </div>
-        <div className='grid grid-cols-4 gap-3 mt-8 mb-4 mobile:grid-cols-2'>
+        <div className='grid gap-3 mt-8 mb-4 mobile:grid-cols-2 desktop:grid-cols-4'>
           <CollectionCard
             collectionName='Trending this week'
             totalCollection={30}
